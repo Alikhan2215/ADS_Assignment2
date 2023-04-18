ea7124cd170a680ba3d36a4159bd38a2ce65d0ad
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-18 22:46:13 +0600

    Created "MyArrayList" class. Defined private instance variable of type Object[]. Defined int variable size.

---
+++ src/MyArrayList.java
@@ -0,0 +1,7 @@
+public class MyArrayList<T> implements MyList {
+    //1. Define a private instance variable of type Object[] to hold the elements of the list.
+    private Object[] array;
+    //2. Define an int variable called size to keep track of the number of elements in the list.
+    private int size = 0;
+
+}
