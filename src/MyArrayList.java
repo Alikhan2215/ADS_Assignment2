8c3161eefb40ca3866108141c0149097c4b5a8f6
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-19 15:44:46 +0600

    Implemented method remove. Added checkIndex method to not repeat myself

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -4,6 +4,11 @@ public class MyArrayList<T> implements MyList<T> {
     //3. Define an int variable called size to keep track of the number of elements in the list.
     private int size = 0;
 
+    public MyArrayList(){
+        this.array = new Object[5];
+        this.size = 0;
+    }
+
     //4. Implement the add(E element)
     @Override
     public void add(T item){
@@ -27,13 +32,37 @@ public class MyArrayList<T> implements MyList<T> {
     @Override
     public T get(int index){
         //If index is not in boundaries -> throw exception
-        if (index < 0 || index >= this.size){
-            throw new IndexOutOfBoundsException();
-        }
+        checkIndex(index);
         //Return index of an array
         return (T) array[index];
     }
 
+    //6. Implement the remove(int index) method
+    @Override
+    public T remove(int index){
+        //If index is not in boundaries -> throw exception
+        checkIndex(index);
+        //Removing the element at the specified index, shifting all subsequent elements to the left by one position
+        Object[] newArray = new Object[this.size - 1];
+        for (int i = index + 1; i < this.size; i++){
+            newArray[i-1] = array[i];
+        }
+
+        array = newArray;
+        //decrementing the size variable.
+        this.size--;
+
+        return (T) array;
+
+    }
+
+    // Added checkIndex method to not repeat myself in method 'remove'
+    public void checkIndex(int index){
+        //If index is not in boundaries -> throw exception
+        if (index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
 
 
 }
