9cf4be089a88254912d7f81c70145523b191f63c
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-19 14:24:46 +0600

    Implemented method add

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,7 +1,27 @@
-public class MyArrayList<T> implements MyList {
-    //1. Define a private instance variable of type Object[] to hold the elements of the list.
+public class MyArrayList<T> implements MyList<T> {
+    //2. Define a private instance variable of type Object[] to hold the elements of the list.
     private Object[] array;
-    //2. Define an int variable called size to keep track of the number of elements in the list.
+    //3. Define an int variable called size to keep track of the number of elements in the list.
     private int size = 0;
+    //4. Implement the add(E element)
+    @Override
+    public void add(T item){
+        //First checking if the size of the array is large enough to accommodate the new element
+        if (this.size == array.length){
+            //If it is not, create a new array with double the size of the original array
+            Object[] newArray = new Object[array.length * 2];
+
+            //Copy over the elements from the original array
+            for (int i = 0; i < this.size; i++){
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
+        }
+
+        //simply add the new element to the end of the array and increment the size variable
+        array[this.size] = item;
+        this.size++;
+    }
 
 }
