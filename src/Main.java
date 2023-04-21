ad3f3ef4d7e1a2c26f3f637f3ab7926a5b5d773b
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-21 18:58:15 +0600

    Finished checking all the methods in main

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,40 @@ public class Main {
         arrayList.add("Clown");
         System.out.println("Element '"+ arrayList.remove(0)+ "' was removed");
         System.out.println("Element at the required index is '" + arrayList.get(0) + "'");
-        System.out.println("'Clown' is at the index " + arrayList.indexOf("Clown"));
         arrayList.clear();
         arrayList.add("Coca-Cola");
         System.out.println("First element of update array is: "+ arrayList.get(0));
         System.out.println("Size of an array is: " + arrayList.size());
 
+        if(arrayList.contains("Coca-Cola")){
+            System.out.println("Coca-Cola is in array");
+        }
+        else{
+            System.out.println("Coca-Cola is not in array");
+        }
+
+        if(arrayList.remove("Coca-Cola")){
+            System.out.println("Item 'Coca-Cola was removed'");
+        }
+        else{
+            System.out.println("Item 'Coca-Cola was not removed'");
+        }
+
         MyArrayList<Integer> intList = new MyArrayList();
         intList.add(6);
         intList.add(2);
         intList.add(3);
+        intList.add(2);
+        System.out.print("List of integers: ");
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.print(intList.get(i) + " "); // print each element in the list
+        }
+        System.out.println("");
+        System.out.println("First encounter with required item is at index: " + intList.indexOf(2));
+        System.out.println("Last encounter with required item is at index: " + intList.lastIndexOf(2));
+
         intList.sort();
-        System.out.print("Sorted list: ");
+        System.out.print("Sorted list of integers: ");
         for (int i = 0; i < intList.size(); i++) {
             System.out.print(intList.get(i) + " "); // print each element in the list
         }
