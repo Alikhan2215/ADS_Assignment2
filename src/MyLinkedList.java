0dae226d60858c73ecc1b98a5a5b773dd816612f
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-21 23:29:50 +0600

    Implemented the add(E element method)

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,6 +1,6 @@
 public class MyLinkedList<E> implements MyList<E> {
 
-    //Define a private inner class called Node
+    //2. Define a private inner class called Node
     private class Node{
         //contains an element of type E
         E element;
@@ -14,10 +14,33 @@ public class MyLinkedList<E> implements MyList<E> {
             this.prev = prev;
         }
     }
-    //Define a private instance variable called head
+    //3. Define a private instance variable called head
     private Node head;
 
-    //Define a private instance variable called tail
+    //4. Define a private instance variable called tail
     private Node tail;
+
+    //5. Define an int variable called size to keep track of the number of elements in the list
+    private int size;
+
+    //6. Implement the add(E element) method
+    @Override
+    public void add(E element){
+        // Creating a new Node with the specified element, setting its next reference to null
+        // (since it will be the new tail), and its previous reference to the current tail
+        Node newNode = new Node(element, null, tail);
+        // If the list is empty, set both the head and tail references to the new node
+        if (tail == null){
+            head = newNode;
+        }
+        // Otherwise, set the next reference of the current tail to the new node and update the tail
+        // reference to the new node
+        else{
+            tail.next = newNode;
+        }
+        tail = newNode;
+        // Finally, increment the size variable
+        size++;
+    }
 
 }
