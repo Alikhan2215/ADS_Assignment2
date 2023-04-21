1243195896919ca7ca58b8bf8796174e4c066954
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-21 22:35:12 +0600

    Created MyLinkedList class. Defined private inner class Node which contains an element of type E and references to the next and previous nodes in the list.

---
+++ src/MyLinkedList.java
@@ -0,0 +1,20 @@
+public class MyLinkedList<E> implements MyList<E> {
+
+    //Define a private inner class called Node
+    private class Node{
+        //contains an element of type E
+        E element;
+        Node next;
+        Node prev;
+
+        //references to the next and previous nodes in the list.
+        Node(E element, Node next, Node prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+
+
+}
