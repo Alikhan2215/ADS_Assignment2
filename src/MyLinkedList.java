84bddefb366adc4fa4b552d4f0e4b2d922f1cf04
Alikhan2215 <dochshanov_alikhan@mail.ru>
2023-04-23 22:37:16 +0600

    Implemented the remove(int index) method

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -77,4 +77,33 @@ public class MyLinkedList<E> implements MyList<E> {
     }
 
 
+    //8. Implement the remove(int index) method
+    @Override
+    public E remove(int index){
+        checkIndex(index);
+
+        // Traverse the list to the node at the specified index
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        // Update the next and previous references of the surrounding nodes to remove the node from the list
+        if (current.prev == null) {
+            head = current.next;
+        } else {
+            current.prev.next = current.next;
+        }
+
+        if (current.next == null) {
+            tail = current.prev;
+        } else {
+            current.next.prev = current.prev;
+        }
+
+        size--;
+        return current.element;
+    }
+
+
 }
